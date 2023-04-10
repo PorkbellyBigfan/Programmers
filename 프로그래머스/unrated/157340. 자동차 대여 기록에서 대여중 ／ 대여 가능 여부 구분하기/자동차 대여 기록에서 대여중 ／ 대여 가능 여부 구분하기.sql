9c0bd18f7cead1945d98e02cd82058bafdd265ae
Hyeongjun Kim <102857959+PorkbellyBigfan@users.noreply.github.com>
2023-04-10 16:11:48 +0900

    Update 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql

--- 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -13,5 +13,4 @@ FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
 GROUP BY CAR_ID
 ORDER BY CAR_ID DESC;
 
-# 여기서 문제는 GROUP BY CAR_ID에 있는데 같은 CAR_ID라도 여러번 대여가 가능한데 AVAILABILITY는 그걸 반영하지 못함
-# 서브쿼리를 활용해서 CASE문을 좀 수정해야한다.
+# 나의 문제 풀이 과정과 해설 https://porkbellyyam.tistory.com/55
